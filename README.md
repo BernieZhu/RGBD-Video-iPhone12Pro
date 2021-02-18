4ec865d086baf8c3a5f77fcfe4c629152c034479
BernieZhu <berniezhu@outlook.com>
2021-02-17 23:57:29 -0500

    load_depth

--- README.md
+++ README.md
@@ -7,9 +7,8 @@
 Library - Export - r3d - Share  
 
 ## Process the Videos
-Change the path in r3d_process.py to the folder that contains r3d files.  
 python r3d_process.py \[options\]
-
+--path: Folder that contains r3d files.  
 --depth: Decode depth file.  
 --video: Save RGB and depth video.  
 --quiet: Disable logs.  
@@ -19,8 +18,9 @@ python r3d_process.py \[options\]
 metadata: Binary file. K is the intrinsic matrix [fx, 0, 0, s, fy, 0, x0, y0, 1].  
 
 Folder: rgbd  
-frame#.conf  
-frame#.jpg  
+frame#.jpg: 256\*192 or 960\*720 rgb.  
 frame#.depth: Original depth (lzfse).  
-frame#.depth_new: Depth map (float32), use depth2image() to decode.  
+frame#.depth_new: Depth map (float32), 256\*192. Each value is the actual depth value in meters. USE THIS ONE.  
 frame#.depth_new.jpg: Depth map for visualization.  
+frame#.conf: Confidence map for each frame. The size is the same as the depth map and for each pixel of the depth map it contains an uint8 number in the range 0-2, which suggest the confidence that the sensed LiDAR depth is "correct". In other words, it is a measure of depth data quality.  
+
--- r3d_process.py
+++ r3d_process.py
@@ -5,8 +5,15 @@ import struct
 import os
 import sys
 import argparse
+import liblzfse
 
-path = '/Users/zhu/Desktop/Salad/'
+def load_depth(filepath):
+    with open(filepath, 'rb') as depth_fh:
+        raw_bytes = depth_fh.read()
+        decompressed_bytes = liblzfse.decompress(raw_bytes)
+        depth_img = np.frombuffer(decompressed_bytes, dtype=np.float32)
+    depth_img = depth_img.reshape((640, 480))
+    return depth_img
 
 def depth2image(filename, nx, ny):
     f = open(filename, "rb")
@@ -21,33 +28,43 @@ def depth2image(filename, nx, ny):
 
 def get_video(folder, typename):
     if (typename == 'depth' or typename == 'depth_new'):
-        typename = 'depth_new.jpg'
+        typename = 'depth.jpg'
     in_folder = os.path.join(folder, 'rgbd')
     out = os.path.join(folder, typename+'.mp4')
     os.system('ffmpeg -loglevel quiet -threads 2 -y -r 30 -i '+in_folder+'/%d.'+typename+' '+out)
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--path', required=True)
     parser.add_argument('--depth', action='store_const', const='depth')
     parser.add_argument('--video', action='store_const', const='video')
     parser.add_argument('--quiet', action='store_const', const='quiet')
     args = parser.parse_args()
 
+    if(not os.path.exists(args.path)):
+        print('ERROR: Wrong path!')
+        return
+    if(os.listdir(args.path)==[]):
+        print('ERROR: Empty folder!')
+        return
     # unzip r3d
-    for r3d in os.listdir(path):
+    for r3d in os.listdir(args.path):
         if(r3d[-4:] == '.r3d'):
-            os.system('unzip -q -n '+os.path.join(path, r3d)+' -d '+path+'/'+r3d[:-4])
-
+            os.system('unzip -q -n '+os.path.join(args.path, r3d)+' -d '+args.path+'/'+r3d[:-4])
+            os.system('rm '+os.path.join(args.path, r3d[:-4])+'/sound.aac')
     # decoding depth files
-    for i in os.listdir(path):
-        folder = os.path.join(path, i)
+    for i in os.listdir(args.path):
+        folder = os.path.join(args.path, i)
         if (os.path.isdir(folder)):
             rgbd_folder = os.path.join(folder, 'rgbd')
             if(args.depth):
                 for j in os.listdir(rgbd_folder):
                     if(j[-6:] == '.depth'):
-                        depth = os.path.join(rgbd_folder, j)
-                        os.system('lzfse -decode -i '+depth+' -o '+depth+'_new')
+                        depth_path = os.path.join(rgbd_folder, j)
+                        os.system('lzfse -decode -i '+depth_path+' -o '+depth_path+'_new')
+                        if(args.depth):
+                            depth_img = load_depth(depth_path)
+                            cv2.imwrite(depth_path+'.jpg', depth_img*255)
                 if (not args.quiet): print('Depth data saved at '+rgbd_folder)
                     
             if(args.video):
@@ -55,14 +72,6 @@ def main():
                 get_video(folder, 'jpg')
                 if (not args.quiet): print('RGB video saved at '+folder)
                 if(args.depth):
-                    # save depth maps
-                    for j in os.listdir(rgbd_folder):
-                        if(j[-4:] == '_new'):
-                            depth = os.path.join(rgbd_folder, j)
-                            depthmap = depth2image(depth, 256, 192)
-                            plt.imsave(depth+'.jpg', depthmap, format='jpg', cmap=plt.cm.jet)
-                    if (not args.quiet): print('Depth maps saved at '+rgbd_folder)
-
                     # depth video
                     get_video(folder, 'depth_new')
                     if (not args.quiet): print('Depth video saved at '+folder)
